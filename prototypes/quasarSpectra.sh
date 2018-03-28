af78fb89fdb8b86b65e17f22e96e7bad37a4d7b3
Hongcheng Zhu <hcz@fb.com>
2018-03-28 03:56:29 -0700

    update

--- prototypes/quasarSpectra.sh
+++ prototypes/quasarSpectra.sh
@@ -2,7 +2,9 @@
 
 set -ex
 
-make QuasarSpectraLinearRegMain
-./QuasarSpectraLinearRegMain
+# make QuasarSpectraLinearRegMain
+# ./QuasarSpectraLinearRegMain
 
 python data_visualizer.py < data/quasar_train_visual.csv
+python data_visualizer.py < data/test_example_1.csv
+python data_visualizer.py < data/test_example_6.csv
