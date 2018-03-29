1c765fd7c5850f34fe37b56355f686078f8ba511
Hongcheng Zhu <hcz@fb.com>
2018-03-29 02:02:22 -0700

    Add a visualize python file

--- prototypes/quasarSpectra.sh
+++ prototypes/quasarSpectra.sh
@@ -5,6 +5,6 @@ set -ex
 # make QuasarSpectraLinearRegMain
 # ./QuasarSpectraLinearRegMain
 
-python data_visualizer.py < data/quasar_train_visual.csv
-python data_visualizer.py < data/test_example_1.csv
-python data_visualizer.py < data/test_example_6.csv
+python QuasarSpectraVisualizer.py < data/quasar_train_visual.csv
+python QuasarSpectraVisualizer.py < data/test_example_1.csv
+python QuasarSpectraVisualizer.py < data/test_example_6.csv
