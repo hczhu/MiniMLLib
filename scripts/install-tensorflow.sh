4da983a4145519c397a5b25786eef24b736c469a
Kezun Sima <simakezun@outlook.com>
2018-05-01 12:35:07 -0700

    update

--- scripts/install-tensorflow.sh
+++ scripts/install-tensorflow.sh
@@ -2,14 +2,17 @@
 
 set -ex
 
-git clone https://github.com/tensorflow/tensorflow 
 sudo apt-get install pkg-config zip g++ zlib1g-dev unzip python
 wget https://github.com/bazelbuild/bazel/releases/download/0.12.0/bazel-0.12.0-installer-linux-x86_64.sh
 chmod a+x bazel-0.12.0-installer-linux-x86_64.sh 
 sudo ./bazel-0.12.0-installer-linux-x86_64.sh
 sudo apt-get install python3-numpy python3-dev python3-pip python3-wheel
+sudo -H pip3 install --upgrade setuptools
+sudo -H pip3 install --upgrade pip
+
+git clone https://github.com/tensorflow/tensorflow 
 cd tensorflow 
-echo -e '\n\n/usr/lib/python3/dist-packages\n\n\nn\nn\n\n\n\n\n\n\n\n\n\n' | ./configure 
+# echo -e '\n\n/usr/lib/python3/dist-packages\n\n\nn\nn\n\n\n\n\n\n\n\n\n\n' | ./configure 
 bazel build --config=opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 //tensorflow/tools/pip_package:build_pip_package
 
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
