765ee2b94056f9929a7f4b3d5dab3c7f57cdf056
Kezun Sima <simakezun@outlook.com>
2018-05-02 11:59:55 -0700

    make kaggle submission

--- py/mnist_ffn.py
+++ py/mnist_ffn.py
@@ -3,9 +3,12 @@ import numpy as np
 import sys
 from datetime import datetime
 
-def readData(images_file, labels_file):
-    x = np.loadtxt(images_file, delimiter=',')
-    y = np.loadtxt(labels_file, delimiter=',')
+def readData(csv_file, has_labels = True):
+    data = np.loadtxt(csv_file, delimiter=',', skiprows=1)
+    if not has_labels:
+        return data, None
+    x = data[:, 1:]
+    y = data[:, 0]
     return x, y
 
 def softmax(x):
@@ -20,12 +23,17 @@ def softmax(x):
 def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x.clip(-100, 100)))
 
-def nn_train(trainData, trainLabels, devData, devLabels, testData, testLabels):
+def one_hot_labels(labels):
+    one_hot_labels = np.zeros((labels.size, 10))
+    one_hot_labels[np.arange(labels.size),labels.astype(int)] = 1
+    return one_hot_labels
+
+def nn_train(trainData, trainLabels, devData, devLabels, testData):
     (m, n) = trainData.shape
     O = trainLabels.shape[1]
     num_hidden = 300
     learning_rate = 5
-    B = 1000
+    B = 256
     W1 = np.random.randn(num_hidden, n)
     B1 = np.zeros(num_hidden, dtype=float)
 
@@ -37,28 +45,27 @@ def nn_train(trainData, trainLabels, devData, devLabels, testData, testLabels):
         a1 = sigmoid(z1)
         z2 = np.matmul(W2, a1) + B2[:, np.newaxis]
         a2 = softmax(z2)
-        accuracy = 1.0 * np.sum((np.argmax(a2, 0) == np.argmax(y, 0)).astype(int)) / y.shape[1]
+        pred = np.argmax(a2, 0)
+        accuracy = 1.0 * np.sum((pred == np.argmax(y, 0)).astype(int)) / y.shape[1]
         xen = -np.sum(y * np.log(a2 + 1e-20)) / y.shape[1]
-        return z1, a1, z2, a2, xen, accuracy
+        return z1, a1, z2, a2, xen, accuracy, pred
     def report(name, x, y):
-        _, _, _, _, xen, accuracy = forward(x.T, y.T)
+        _, _, _, _, xen, accuracy, _ = forward(x.T, y.T)
         sys.stderr.write('{}: CEL = {:.12f} accuracy = {:.4f}\n'.format(
             name,
             xen,
             accuracy)) 
         sys.stderr.flush()
         return (xen, accuracy)
-    dev_stats = []
-    train_stats = []
     E = 30
-    print('Epoch {}'.format(','.join([str(e) for e in range(E)])))
+    train_log_loss = 0
     for epoch in range(1, E + 1):
         for b in range(0, m, B):
             # Reference: http://cs229.stanford.edu/notes/cs229-notes-backprop.pdf
             a0 = trainData[b:(b + B), :].T
             S = a0.shape[1]
             y = trainLabels[b:(b + B), :].T
-            z1, a1, z2, a2, xen, _ = forward(a0, y)
+            z1, a1, z2, a2, xen, _, _ = forward(a0, y)
             dz2 = a2 - y
             dw2 = np.matmul(dz2, a1.T)
 
@@ -70,29 +77,23 @@ def nn_train(trainData, trainLabels, devData, devLabels, testData, testLabels):
             B2 -= (learning_rate / S) * dz2.sum(1)
             W1 -= (learning_rate / S) * dw1 + (2 * L * learning_rate * W1)
             B1 -= (learning_rate / S) * dz1.sum(1)
-        dev_stats.append(report('dev at epoch #{}'.format(epoch), devData, devLabels))
-        train_stats.append(report('train at epoch #{}'.format(epoch), trainData, trainLabels))
-
-    print('train-CEL {}'.format(','.join(['{:.2f}'.format(x[0]) for x in train_stats])))
-    print('dev-CEL {}'.format(','.join(['{:.2f}'.format(x[0]) for x in dev_stats])))
-
-    print('dev-accuracy {}'.format(','.join(['{:.4f}'.format(x[1]) for x in dev_stats])))
-    print('train-accuracy {}'.format(','.join(['{:.4f}'.format(x[1]) for x in train_stats])))
-    report('Test', testData, testLabels)
-            
-def one_hot_labels(labels):
-    one_hot_labels = np.zeros((labels.size, 10))
-    one_hot_labels[np.arange(labels.size),labels.astype(int)] = 1
-    return one_hot_labels
+            train_log_loss = train_log_loss * 0.9 + 0.1 * xen
+            sys.stderr.write('Smoothed training log-loss: {:.4f}\n'.format(train_log_loss))
+        report('Dev at epoch #{}'.format(epoch), devData, devLabels)
+    report('Train', trainData, trainLabels)
+    _, _, _, _, _, _, pred = forward(testData.T, one_hot_labels(np.zeros(testData.shape[0])).T)
+    print('ImageId,Label')
+    for i in range(pred.size):
+        print('{},{}'.format(i + 1, pred[i]))            
 
 def main():
     begin = datetime.now()
-    np.random.seed(100)
+    np.random.seed(135)
     suffix = ''
-    trainData, trainLabels = readData('data/images_train.csv' + suffix, 'data/labels_train.csv' + suffix)
+    trainData, trainLabels = readData('mnist-data/train.csv')
     trainLabels = one_hot_labels(trainLabels)
-    N = 60000
-    DN = 10000
+    N = trainData.shape[0]
+    DN = 2000
     p = np.random.permutation(N)
     trainData = trainData[p,:]
     trainLabels = trainLabels[p,:]
@@ -107,17 +108,17 @@ def main():
     trainData = (trainData - mean) / std
     devData = (devData - mean) / std
 
-    testData, testLabels = readData('data/images_test.csv', 'data/labels_test.csv')
-    testLabels = one_hot_labels(testLabels)
+    testData, _ = readData('mnist-data/test.csv', False)
     testData = (testData - mean) / std
+
     sys.stderr.write('Loaded all data.\n')
     sys.stderr.flush()
     datatime = datetime.now()
     sys.stderr.write('Data loading time = {:d} seconds\n'.format((datatime - begin).seconds))
     sys.stderr.flush()
-    nn_train(trainData, trainLabels, devData, devLabels, testData, testLabels)
+    nn_train(trainData, trainLabels, devData, devLabels, testData)
     sys.stderr.write('Training time = {:d} seconds\n'.format((datetime.now() - datatime).seconds))
-    sys.stderr.flush()
+    siys.stderr.flush()
 
 if __name__ == '__main__':
     main()
