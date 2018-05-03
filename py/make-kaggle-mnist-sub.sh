bea8373746318d5bf7e98fe83f5bd153af4bae3c
Kezun Sima <simakezun@outlook.com>
2018-05-02 21:42:39 -0700

    update

--- py/make-kaggle-mnist-sub.sh
+++ py/make-kaggle-mnist-sub.sh
@@ -6,5 +6,6 @@ fi
 log_file='/tmp/mnist.log'
 rm -fr $log_file
 touch $log_file
-python3 mnist_ffn.py 2> $log_file | tee mnist-data/sub.csv &
+python3 mnist_ffn.py 2> $log_file | tee mnist-data/sub.csv
 tail -f $log_file
+kaggle competitions submit -c digit-recognizer -f mnist-data/sub.csv
